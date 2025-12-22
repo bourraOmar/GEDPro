ea2f11657888d276865e78371093e4be45a16e50
bourraOmar <bwrtmr@gmail.com>
2025-12-22 16:15:37 +0100

    Add AuthController with registration, login, and profile retrieval endpoints

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Request, Post, UseGuards, Body, Get } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { ApiTags, ApiBody, ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  @ApiBody({ type: RegisterDto })
+  async register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  @ApiBody({ type: LoginDto })
+  async login(@Request() req, @Body() loginDto: LoginDto) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  @ApiBearerAuth()
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
