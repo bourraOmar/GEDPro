58d64c9fa415225ff15465ec704a7d9e7e614a65
bourraOmar <bwrtmr@gmail.com>
2025-12-23 16:40:37 +0100

    Remove comment about password storage in UsersService

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -8,7 +8,7 @@ export class UsersService {
     {
       userId: 1,
       username: 'john',
-      password: 'changeme', // In a real app, store hashed passwords
+      password: 'changeme', 
     },
     {
       userId: 2,
