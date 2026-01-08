4acdd792549647af5372c15b5fbb279f5994f94d
bourraOmar <bwrtmr@gmail.com>
2026-01-08 09:17:04 +0100

    Add CompaniesModule to AppModule imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CompaniesModule } from './companies/companies.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     // MongooseModule.forRoot('mongodb://lacalhost/gedpro'),
     AuthModule,
     UsersModule,
+    CompaniesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
