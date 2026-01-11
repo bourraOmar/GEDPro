efacf2607da46ff37465972937f3d17400fdae72
bourraOmar <bwrtmr@gmail.com>
2026-01-11 17:07:06 +0100

    fix: correct formatting and enable CORS in main application bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,14 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('HR GED API')
     .setDescription('The HR Management API description')
     .setVersion('1.0')
-    .addBearerAuth() 
+    .addBearerAuth()
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
